692590e0f3035c7f65422401943e2e0ce502d48c
Zak B. Elep <zakame@zakame.net>
2013-10-18 12:54:47 +0800

    Add fullscreen magit-status
    Taken from http://whattheemacsd.com/setup-magit.el-01.html

--- init.el
+++ init.el
@@ -291,6 +291,18 @@
 (add-to-list 'load-path "~/.emacs.d/magit/contrib")
 (require 'magit)
 
+; full screen magit
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+(defun magit-quit-session ()
+  "Restores the previous window configuratoin and kills the magit buffer."
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 ;; nXhtml
 (load "~/.emacs.d/nxhtml/autostart.el")
 
