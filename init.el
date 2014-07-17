288d66666a192b80b38b23c1ada379a920b6586e
Zak B. Elep <zak.elep@yoyo.ph>
2014-07-17 18:16:32 +0800

    Fix typo

--- init.el
+++ init.el
@@ -218,7 +218,7 @@
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/previous-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 
