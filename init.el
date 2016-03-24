0ed0862a8031548d04072bd20754775b97d7d6b6
Zak B. Elep <zakame@zakame.net>
2016-03-24 13:31:36 +0800

    Use diff-hl-flydiff-mode on Emacs 24.4 up

--- init.el
+++ init.el
@@ -719,6 +719,8 @@
   (global-diff-hl-mode)
   (setq diff-hl-side 'left)
   (diff-hl-margin-mode)
+  (unless (version<= emacs-version "24.4")
+    (diff-hl-flydiff-mode))
   (eval-after-load "magit"
     '(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)))
 
