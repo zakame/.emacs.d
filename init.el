532563ca2285ed83cc730ace5ce13d00fbdae3da
Zak B. Elep <zakame@zakame.net>
2018-03-13 13:11:40 +0800

    Let powerline display the git glyph even in GUI

--- init.el
+++ init.el
@@ -286,6 +286,7 @@
 (use-package powerline
   :ensure t
   :config
+  (setq powerline-gui-use-vcs-glyph t)
   ;; I don't like the recent powerline window focus change, so undo it
   (remove-hook 'focus-out-hook 'powerline-unset-selected-window)
   (add-hook 'focus-out-hook 'powerline-set-selected-window)
