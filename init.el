7013f410eed64d6fd74452c89848d1f34eac15e5
Zak B. Elep <zakame@zakame.net>
2021-08-31 11:58:45 +0800

    Revert "Undo powerline window focus "fix""
    No longer an issue for me.
    
    This reverts commit 818b5e364f40424fb47549a85dd275b8e307c255.

--- init.el
+++ init.el
@@ -304,9 +304,6 @@
   :config
   (unless (eq window-system 'ns)
     (setq powerline-gui-use-vcs-glyph t))
-  ;; I don't like the recent powerline window focus change, so undo it
-  (remove-hook 'focus-out-hook 'powerline-unset-selected-window)
-  (add-hook 'focus-out-hook 'powerline-set-selected-window)
   (powerline-default-theme))
 
 ;; Moe-Theme
